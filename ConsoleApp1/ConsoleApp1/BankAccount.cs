0d31b2956a0b2ca3210017d7275140bcc8a5168a
BuildTools <unconfigured@null.spigotmc.org>
2018-03-12 19:57:49 -0400

    first time

---
+++ ConsoleApp1/ConsoleApp1/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class BankAccount
+    {
+        public string Number { get;}
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+        private static int accountNumberSeed = 1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            this.Owner = name;
+            makeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+        public void makeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawl must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+        public void makeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+        public string getAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+            report.AppendLine("Date\t\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("John", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance:C2} initial balance.");
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+            account.makeWithdrawal(500, DateTime.Now, "rent payment");
+            Console.WriteLine(account.Balance);
+            account.makeDeposit(100, DateTime.Now, "friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(account.getAccountHistory());
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+            try
+            {
+                var invalidAccount = new BankAccount("invalid",-55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("exception w/ creating acct with neg balance");
+                Console.WriteLine(e.ToString());
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+            try
+            {
+                account.makeWithdrawal(750, DateTime.Now, "Attempting to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+        }
+    }
+}
